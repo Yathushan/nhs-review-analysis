94c18f3027cadf49d0725093ac83285646e008e8
Yathushan <yatmaster@gmail.com>
2022-12-24 12:42:37 +0000

    Filter by clinic, view summary & specific reviews

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,37 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+clinic_metadata = pd.read_csv('filtered_gp_clinics.csv', index_col='Unnamed: 0')
+clinic_reviews = pd.read_csv('gp_reviews.csv', index_col='Unnamed: 0')
+clinic_metadata.rename(columns={"Latitude": "latitude", "Longitude": "longitude"}, inplace=True)
+# clinic_names = clinic_metadata.OrganisationName.unique()
+clinic_names = clinic_reviews.clinic.unique()
+clinic_reviews['rating'] = clinic_reviews['rating'].astype(int)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+option = st.selectbox(
+    "What clinic are you interested in?",
+    clinic_names
+)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+clinic_reviews = clinic_reviews[['clinic', 'date', 'rating', 'title', 'comment']]
+select_clinic = clinic_reviews[clinic_reviews['clinic']==option]
+no_ratings = select_clinic['rating'].count()
+avg_rating = select_clinic['rating'].mean().round(1)
+delta_rating = select_clinic['rating'].iat[0] - select_clinic['rating'].iat[-1]
 
+def highlight(s):
+    if s.rating > 3:
+        return ['background-color: green'] * len(s)
+    elif s.rating < 3:
+        return ['background-color: red'] * len(s)
+    else:
+        return ['background-color: white'] * len(s)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+col1, col2 = st.columns(2)
+col1.metric(label="Number of Ratings", value=no_ratings)
+col2.metric(label="Average Rating", value=avg_rating, delta=delta_rating.item())
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.map(data=clinic_metadata[clinic_metadata['OrganisationName']==option], zoom=None, use_container_width=True)
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.table(clinic_reviews[clinic_reviews['clinic']==option].style.apply(highlight, axis=1))
