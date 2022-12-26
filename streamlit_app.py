7e8d9fe85fdc637ae6d5dec203b654440f3e350d
Yathushan <yatmaster@gmail.com>
2022-12-26 18:41:02 +0000

    Moved to folium map & added time series chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,28 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
+
+# from collections import namedtuple
+
+import math
+import folium
+from streamlit_folium import st_folium
+
+import altair as alt
+
 import streamlit as st
 
 
 clinic_metadata = pd.read_csv('filtered_gp_clinics.csv', index_col='Unnamed: 0')
 clinic_reviews = pd.read_csv('gp_reviews.csv', index_col='Unnamed: 0')
-clinic_metadata.rename(columns={"Latitude": "latitude", "Longitude": "longitude"}, inplace=True)
+# clinic_metadata.rename(columns={"Latitude": "latitude", "Longitude": "longitude"}, inplace=True)
 # clinic_names = clinic_metadata.OrganisationName.unique()
 clinic_names = clinic_reviews.clinic.unique()
 clinic_reviews['rating'] = clinic_reviews['rating'].astype(int)
 
-option = st.selectbox(
-    "What clinic are you interested in?",
-    clinic_names
-)
+with st.sidebar:
+    option = st.selectbox(
+        "What clinic are you interested in?",
+        clinic_names
+    )
 
 clinic_reviews = clinic_reviews[['clinic', 'date', 'rating', 'title', 'comment']]
 select_clinic = clinic_reviews[clinic_reviews['clinic']==option]
@@ -35,6 +42,44 @@ col1, col2 = st.columns(2)
 col1.metric(label="Number of Ratings", value=no_ratings)
 col2.metric(label="Average Rating", value=avg_rating, delta=delta_rating.item())
 
-st.map(data=clinic_metadata[clinic_metadata['OrganisationName']==option], zoom=None, use_container_width=True)
+lat = clinic_metadata[clinic_metadata['OrganisationName']==option].Latitude
+log = clinic_metadata[clinic_metadata['OrganisationName']==option].Longitude
+
+this_map = folium.Map(prefer_canvas=True, location=[lat, log], zoom_start=15)
+
+fg = folium.FeatureGroup(name="GP Clinics")
+
+def plotDot(point):
+    '''input: series that contains a numeric named latitude and a numeric named longitude
+    this function creates a CircleMarker and adds it to your feature group'''
+    fg.add_child(
+        folium.CircleMarker(
+            location=[point.Latitude, point.Longitude],
+            radius=2,
+            weight=5,
+            tooltip=point.OrganisationName
+        )
+    )
+
+#use df.apply(,axis=1) to "iterate" through every row in your dataframe
+clinic_metadata.apply(plotDot, axis = 1)
+
+st_map = st_folium(
+    this_map,
+    feature_group_to_add=fg,
+    width=700,
+    height=450
+)
+
+time_ratings = alt.Chart(clinic_reviews[clinic_reviews['clinic']==option]).mark_line().encode(
+    x='yearmonth(date)',
+    y='mean(rating)',
+    tooltip=['mean(rating)']
+).properties(
+    width=800,
+    height=300
+)
+
+st.altair_chart(time_ratings, use_container_width=True)
 
 st.table(clinic_reviews[clinic_reviews['clinic']==option].style.apply(highlight, axis=1))
