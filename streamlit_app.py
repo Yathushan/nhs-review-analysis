abe2143034ac1381bf89d46e82c41936883f934f
Yathushan <yatmaster@gmail.com>
2022-12-29 18:09:02 +0000

    Remove unnecessary processing and added caching

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,50 +6,15 @@ from streamlit_folium import st_folium
 
 import altair as alt
 
-from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
+import matplotlib
 from wordcloud import WordCloud
 
 import streamlit as st
 
 
-clinic_metadata = pd.read_csv('filtered_gp_clinics.csv', index_col='Unnamed: 0')
-clinic_reviews = pd.read_csv('gp_reviews.csv', index_col='Unnamed: 0')
 
-clinic_names = clinic_reviews.clinic.unique()
-clinic_reviews['rating'] = clinic_reviews['rating'].astype(int)
-
-with st.sidebar:
-    option = st.selectbox(
-        "What clinic are you interested in?",
-        clinic_names
-    )
-
-clean_clinic_reviews = clinic_reviews[['clinic', 'date', 'rating', 'title', 'comment']]
-select_clinic = clinic_reviews[clinic_reviews['clinic']==option]
-no_ratings = select_clinic['rating'].count()
-avg_rating = select_clinic['rating'].mean().round(1)
-delta_rating = select_clinic['rating'].iat[0] - select_clinic['rating'].iat[-1]
-
-def highlight(s):
-    if s.rating > 3:
-        return ['background-color: green'] * len(s)
-    elif s.rating < 3:
-        return ['background-color: red'] * len(s)
-    else:
-        return ['background-color: white'] * len(s)
-
-col1, col2 = st.columns(2)
-col1.metric(label="Number of Ratings", value=no_ratings)
-col2.metric(label="Average Rating", value=avg_rating, delta=delta_rating.item())
-
-lat = clinic_metadata[clinic_metadata['OrganisationName']==option].Latitude
-log = clinic_metadata[clinic_metadata['OrganisationName']==option].Longitude
-
-this_map = folium.Map(prefer_canvas=True, location=[lat, log], zoom_start=15)
-
-fg = folium.FeatureGroup(name="GP Clinics")
-
+# Plots coordinates on a map
 def plotDot(point):
     '''input: series that contains a numeric named latitude and a numeric named longitude
     this function creates a CircleMarker and adds it to your feature group'''
@@ -62,33 +27,20 @@ def plotDot(point):
         )
     )
 
-#use df.apply(,axis=1) to "iterate" through every row in your dataframe
-clinic_metadata.apply(plotDot, axis = 1)
+# Colours Dataframes based on rating
+def highlight(s):
+    if s.rating > 3:
+        return ['background-color: green'] * len(s)
+    elif s.rating < 3:
+        return ['background-color: red'] * len(s)
+    else:
+        return ['background-color: white'] * len(s)
 
-st_map = st_folium(
-    this_map,
-    feature_group_to_add=fg,
-    width=700,
-    height=450
-)
-
-time_ratings = alt.Chart(clinic_reviews[clinic_reviews['clinic']==option]).mark_line().encode(
-    x='yearmonth(date)',
-    y='mean(rating)',
-    tooltip=['mean(rating)']
-).properties(
-    width=800,
-    height=300
-)
-
-st.altair_chart(time_ratings, use_container_width=True)
-
-def word_cloud(reviews):
-    word_vectorizer = CountVectorizer(ngram_range=(1,2), analyzer='word')
-    sparse_matrix = word_vectorizer.fit_transform(reviews['comment_wo_stopwords'])
-    frequencies = sum(sparse_matrix).toarray()[0]
-    word_freq = pd.DataFrame(frequencies, index=word_vectorizer.get_feature_names_out(), columns=['frequency'])
-    word_freq.sort_values(['frequency'], ascending=False).head(10)
+# WordCloud generator
+@st.cache(hash_funcs={matplotlib.figure.Figure: hash})
+def word_cloud(reviews, clinic_name=None):
+    if clinic_name != None:
+        reviews = reviews[reviews['clinic']==clinic_name]
 
     text = " ".join(cat.split()[1] for cat in reviews.comment_wo_stopwords)
     word_cloud = WordCloud(collocations = False, background_color = 'white', width=1000, height=500, scale=2).generate(text)
@@ -98,12 +50,66 @@ def word_cloud(reviews):
     plt.axis("off")
     return fig
 
-st.write('General area')
-glob_cloud = word_cloud(clinic_reviews)
-st.pyplot(glob_cloud)
+clinic_metadata = pd.read_csv('filtered_gp_clinics.csv', index_col='Unnamed: 0')
+clinic_reviews = pd.read_csv('gp_reviews.csv', index_col='Unnamed: 0')
 
-st.write('Local clinic')
-local_cloud = word_cloud(clinic_reviews[clinic_reviews['clinic']==option])
-st.pyplot(local_cloud)
+clinic_names = sorted(clinic_reviews.clinic.unique())
+clinic_reviews['rating'] = clinic_reviews['rating'].astype(int)
 
-st.table(clean_clinic_reviews[clean_clinic_reviews['clinic']==option].style.apply(highlight, axis=1))
+with st.sidebar:
+    option = st.selectbox(
+        "What clinic are you interested in?",
+        clinic_names
+    )
+
+select_clinic = clinic_reviews[clinic_reviews['clinic']==option]
+no_ratings = select_clinic['rating'].count()
+avg_rating = select_clinic['rating'].mean().round(1)
+delta_rating = select_clinic['rating'].iat[0] - select_clinic['rating'].iat[-1]
+
+lat = clinic_metadata[clinic_metadata['OrganisationName']==option].Latitude
+log = clinic_metadata[clinic_metadata['OrganisationName']==option].Longitude
+
+# Create Streamlit Page
+st.title("NHS Primary Care Patient Reviews")
+st.markdown("This mini-app allows patients to see how their local GP clinic has been reviewed. It is based on patient reviews scraped from [NHS Services](https://www.nhs.uk/services/gp-surgery/the-ridgeway-surgery/E84068/ratings-and-reviews)")
+
+col1, col2 = st.columns(2)
+col1.metric(label="Number of Ratings", value=no_ratings)
+col2.metric(label="Average Rating", value=avg_rating, delta=delta_rating.item())
+
+this_map = folium.Map(prefer_canvas=True, location=[lat, log], zoom_start=15)
+fg = folium.FeatureGroup(name="GP Clinics")
+clinic_metadata.apply(plotDot, axis = 1)
+st_map = st_folium(
+    this_map,
+    feature_group_to_add=fg,
+    width=700,
+    height=450
+)
+
+st.write("Average Monthly Patient Rating")
+time_ratings = alt.Chart(select_clinic).mark_line().encode(
+    x='yearmonth(date)',
+    y='mean(rating)',
+    tooltip=['mean(rating)']
+).properties(
+    width=800,
+    height=300
+)
+st.altair_chart(time_ratings, use_container_width=True)
+
+general_word_tabs, specific_words_tab = st.tabs(["General", "Specific"])
+
+with general_word_tabs:
+    st.subheader('Common words reported for Harrow')
+    glob_cloud = word_cloud(clinic_reviews)
+    st.pyplot(glob_cloud)
+
+with specific_words_tab:
+    st.subheader('Common words reported for ' + option)
+    local_cloud = word_cloud(clinic_reviews, option)
+    st.pyplot(local_cloud)
+
+with st.expander("See all reviews"):
+    st.table(select_clinic[['clinic', 'date', 'rating', 'title', 'comment']].style.apply(highlight, axis=1))
